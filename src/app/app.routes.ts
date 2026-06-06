0350f306a41075d45d19ab040c7382881cd06fe9
joelcitorueda <ruedafloresjulian722@gmail.com>
2026-06-05 23:13:42 -0400

    Fix URL: quitar rutas Angular + mostrar descripciones en movil + vercel.json SPA

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,10 +1,3 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [
-  { path: '', redirectTo: '/inicio', pathMatch: 'full' },
-  { 
-    path: 'inicio',
-    loadComponent: () => import('./components/hero/hero.component').then(m => m.HeroComponent)
-  },
-  { path: '**', redirectTo: '/inicio' }
-];
+export const routes: Routes = [];
